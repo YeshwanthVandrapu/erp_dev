0382821ded9f77bf27f4dfdb0a48a3b93f11b9a5
YeshwanthVandrapu <yeshwanth.vandrapu@krea.edu.in>
2024-05-31 10:19:41 +0530

    first commit

---
+++ lib/pages/home/view.dart
@@ -0,0 +1,190 @@
+import 'package:erp_dev/utils/print.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:responsive_ui/responsive_ui.dart';
+
+import '../../utils/icons.dart';
+import 'widgets/home_schedule/view.dart';
+import 'widgets/quick_links.dart';
+import 'widgets/student_buddy_card.dart';
+import 'widgets/welcome_widget.dart';
+
+class NewHomeBody extends StatefulWidget {
+  const NewHomeBody({super.key});
+
+  @override
+  State<NewHomeBody> createState() => _NewHomeBodyState();
+}
+
+class _NewHomeBodyState extends State<NewHomeBody> {
+  @override
+  void initState() {
+    super.initState();
+    loadJsonAsset();
+  }
+
+  final List<Map<String, dynamic>> jsonData = [];
+  Future<void> loadJsonAsset() async {
+    final String jsonString = await rootBundle.loadString('res/json/menu.json');
+    var data = jsonDecode(jsonString);
+    jsonData.clear();
+    for (var e in data) {
+      jsonData.add(e);
+    }
+    setState(() {});
+    dPrint(jsonData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double sWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 10,
+        backgroundColor: Colors.white,
+        title: RichText(
+          text: const TextSpan(children: [
+            TextSpan(
+              text: "Dashboard  ",
+              style: TextStyle(
+                fontSize: 24,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+              ),
+            ),
+            WidgetSpan(
+              child: Icon(Icons.settings_suggest_outlined),
+            ),
+          ]),
+        ),
+        actions: sWidth < 400
+            ? []
+            : [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Row(
+                      children: [
+                        Icon(Icons.search),
+                        SizedBox(width: 5),
+                        Text(
+                          'Search',
+                          // style: TextStyle(color: Colors.black, fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+      ),
+      drawer: sWidth <= 600
+          ? Drawer(
+              backgroundColor: const Color(0xff275C9D),
+              // clipBehavior: ,
+              child: ListView(
+                children: jsonData.isEmpty
+                    ? [const CircularProgressIndicator()]
+                    : jsonData.map((data) {
+                        return ListTile(
+                          //style:
+                          title: Text(
+                            data['menu'] ?? "",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(color: Color(0xffBDE2EE)),
+                          ),
+                          leading: CustomMaterialIcon(
+                            data['icons'] ?? "",
+                            color: const Color(0xffBDE2EE),
+                          ),
+                        );
+                      }).toList(),
+              ),
+            )
+          : null,
+      body: SingleChildScrollView(
+        child: Responsive(
+          // crossAxisAlignment = WrapCrossAlignment.start,
+          runSpacing: 50,
+          children: <Widget>[
+            Div(
+              divison: const Division(
+                colXL: 6,
+                colL: 10,
+                colXS: 10,
+                offsetXL: 1,
+                offsetL: 1,
+                offsetM: 1,
+                offsetS: 1,
+                offsetXS: 1,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 40, bottom: 40),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 189, 226, 238),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    constraints:
+                        const BoxConstraints(maxHeight: 300, minHeight: 270),
+                    child: const WelcomeCard(),
+                  ),
+                  // const Cardgrid(),
+                  // Container(
+                  //   margin: const EdgeInsets.only(top: 40, bottom: 40),
+                  //   decoration: BoxDecoration(
+                  //       color: const Color.fromARGB(255, 189, 226, 238),
+                  //       borderRadius: BorderRadius.circular(8),
+                  //       border: Border.all(width: 1, color: Colors.grey)),
+                  //   constraints: const BoxConstraints(maxHeight: 300),
+                  //   child: const TaskListCard(),
+                  // ),
+
+                  Container(
+                    constraints:
+                        const BoxConstraints(maxWidth: 400, maxHeight: 400),
+                    child: const StudentCard(),
+                  ),
+                ],
+              ),
+            ),
+            Div(
+              divison: const Division(
+                colXL: 3,
+                colL: 10,
+                colXS: 10,
+                offsetXL: 1,
+                offsetL: 1,
+                offsetM: 1,
+                offsetS: 1,
+                offsetXS: 1,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 40, bottom: 40),
+                    constraints:
+                        const BoxConstraints(maxHeight: 300, maxWidth: 500),
+                    child: const QuickLinks(),
+                  ),
+                  // Container(
+                  //   margin: const EdgeInsets.only(top: 40, bottom: 40),
+                  //   constraints:
+                  //       const BoxConstraints(maxHeight: 500, maxWidth: 500),
+                  //   child: const CalenderCard(),
+                  // ),
+                  const UpcomingSchedule(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home/widgets/student_buddy_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StudentCard extends StatelessWidget {
+  const StudentCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.network(
+                'https://s3-alpha-sig.figma.com/img/84b9/d56a/724f72eb2c73d3e7560e01a5f0093700?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GQSRtbWppcrV-K1VnMVpyizbsy4tL2KmWbI7aHzxwAi3bMgd826P1ACl6WV0D3PNNFdwUpnsOgEwWu8FupkezHmr-7CibmpfDt8IS2kz5U0NhiUcpGRsJmcaydWiQY0xCCXUPKdmgXfiM5z09ABSRIhRSod1LF03viyaOiVbT8WsQLgSn9Tq1ZUMAaRMj-RHZQzDU~fgNfpRL5EfHrcbSIJPJk3nzwIQWbOW6XzYg4V55fJUxffipdJLmf3EAsQ30TEGedMUzRM99E2hkLSW2i90KGnkulCf-wMmDDGZpvIGhPw-qJmRwtNJZaMLl9swtDezLeMSXQ0YQgBDOZ-5qw__',
+                height: 80,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Billie Rikhari',
+              style: GoogleFonts.urbanist(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              'Student',
+              style: GoogleFonts.urbanist(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 16),
+            RichText(
+              text: TextSpan(
+                  style: GoogleFonts.urbanist(
+                      color: const Color(0xff8F9BB3), fontSize: 12),
+                  children: const [
+                    WidgetSpan(child: Icon(Icons.school_outlined)),
+                    TextSpan(text: "Psychology"),
+                  ]),
+            ),
+            RichText(
+              text: TextSpan(
+                  style: GoogleFonts.urbanist(
+                      color: const Color(0xff8F9BB3), fontSize: 12),
+                  children: const [
+                    WidgetSpan(child: Icon(Icons.mail_outline_rounded)),
+                    TextSpan(text: "sample.email.sias88@krea.ac.in"),
+                  ]),
+            ),
+            const SizedBox(height: 16),
+            Divider(color: Colors.grey[300]),
+            const SizedBox(height: 8),
+            Text(
+              'Say Hi!',
+              style: GoogleFonts.urbanist(
+                fontSize: 16,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
