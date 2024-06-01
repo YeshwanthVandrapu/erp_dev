5fdb81021656e8ede6afe80fc386db7bd56c0a7a
prithvipaddy <prithvipaddy@gmail.com>
2024-06-01 15:08:39 +0530

    changed text in the pop up

--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -215,8 +215,27 @@ class _NewHomeBodyState extends State<NewHomeBody> {
                           fontFamily: GoogleFonts.urbanist().fontFamily,
                           decoration: TextDecoration.none),
                 ),
-                const Text(
-                    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.")
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Dear Student,"),
+                    Text(
+                        "As we gear up to welcome you on campus, we need your support in ensuring a seamless transition for your enrollment as a Krea Student. We require all the information collected in this form to initiate your registration as a bonafide student of the University. Please furnish all the requested information correctly and fill all the sections to the best of your knowledge."),
+                    Text("Important Note: "),
+                    Text(
+                        "(1) Providing incorrect information intentionally or deliberately withholding the requested information from the University can result in the cancellation/suspension of your enrolment status as a student of the University"),
+                    Text(
+                        "(2) To ensure smooth and easy submission, please use this form on a laptop or desktop computer."),
+                    Text.rich(TextSpan(children: [
+                      TextSpan(
+                          text:
+                              "(3) The last deadline to update all the requested information is  "),
+                      TextSpan(
+                          text: "22nd July 2024",
+                          style: TextStyle(fontWeight: FontWeight.bold))
+                    ]))
+                  ],
+                )
               ],
             ),
             actions: [
