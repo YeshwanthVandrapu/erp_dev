1348e67c0dd0d66c16d9bc02317aac397309df59
ArcticMonkey101 <vidisha_ponnappa.sias21@krea.ac.in>
2024-06-26 14:08:51 +0530

    assignRooms updated

--- lib/rooms/room.dart
+++ lib/rooms/room.dart
@@ -1,3 +1,5 @@
+import 'package:erp_dev/rooms/roommate.dart';
+
 List<Map<String, dynamic>> assignRooms(
     List<Map<String, dynamic>> rooms, List<List<dynamic>> matches) {
   List<Map<String, dynamic>> roomsCopy = List.from(rooms);
@@ -7,19 +9,21 @@ List<Map<String, dynamic>> assignRooms(
     for (var room in roomsCopy) {
       if (match[0]['sex'] == room['Category'] &&
           match[0]['school'] == room['School']) {
-        if (match[1] != null) {
-          finalRooms.add({
-            'Student1': match[0]['id'],
-            'Student2': match[1]['id'],
-            'RoomNumber': room['Room Number']
-          });
-        } else {
-          finalRooms.add({
-            'Student1': match[0]['id'],
-            'Student2': '',
-            'RoomNumber': room['Room Number']
-          });
-        }
+        // Create Student objects for PairedStudents
+        Student student1 = Student(student1: match[0]['id']);
+        Student student2 =
+            match[1] != null ? Student(student1: match[1]['id']) : Student();
+
+        // Create PairedStudents object
+        PairedStudents pairedStudents = PairedStudents(
+          roomNumber: room['Room Number'].toString(),
+          students: [student1, student2],
+        );
+
+        // Convert PairedStudents object to JSON and add to finalRooms
+        finalRooms.add(pairedStudents.toJson());
+
+        // Remove room from roomsCopy
         roomsCopy.remove(room);
         break;
       }
